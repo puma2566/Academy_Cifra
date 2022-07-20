7abbe647d4199937d776cf6ac8b9e75f08b1e010
puma2566 <izhboldi@gmail.com>
2022-07-20 09:16:47 +0400

    TicTacToe

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,175 +1,111 @@
-﻿namespace Lesson_2
+﻿namespace test3
 {
     internal class Program
     {
+        static int player = 1;
+
         static void Main(string[] args)
         {
-            char[,] mass = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9'} };
-            for (byte i = 0; i < 3; i++)
+            int w;
+            string? PlayerValue;
+            int ValueConversion;
+            char[] mass = new char[9] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i = 0; i < 9; i++)
             {
-                for (byte j = 0; j < 3; j++)
+                Console.Write($"|_{mass[i]}_");
+                if (i % 3 == 2)
                 {
-                    Console.Write($"_{mass[i, j]}_");
-
-                    if (i != -1 && j != -1)
-                    {
-                        Console.Write("|");
-                    }
-                }
-                Console.Write("\n");
-            }
-
-            int player = 1;
-
-            //Создание метода
-            void game(char v, int z, int c)
-            {
-                if (mass[z, c] == 'X' | mass[z, c] == '0')
-                {
-                    Console.WriteLine("Ячейка занята");
-                }
-                else
-                {
-                    mass[z, c] = v;
-                    player++;
-                }
-
-                for (byte i = 0; i < 3; i++)
-                {
-                    for (byte j = 0; j < 3; j++)
-                    {
-                        Console.Write($"_{mass[i, j]}_");
-
-                        if (i != -1 && j != -1)
-                        {
-                            Console.Write("|");
-                        }
-                    }
+                    Console.Write("|");
                     Console.Write("\n");
                 }
-                return;
             }
 
-
-            // Игровой цикл
-            for (byte k = 0; k < 10; k++)
+            while (mass.Length == 9)
             {
-                if (player % 2 != 0)
+
+                if (player % 2 == 0)
                 {
-                    Console.WriteLine("Ход игрока 1");
+                    Console.WriteLine("Ход игрока :0");
                 }
                 else
                 {
-                    Console.WriteLine("Ход игрока 2");
+                    Console.WriteLine("Ход игрока :x");
                 }
-                string? y = Console.ReadLine();
-                int x = Convert.ToInt32(y);
-                
-                if (x < 1 || x > 9)
+                PlayerValue = Console.ReadLine();
+                ValueConversion = Convert.ToInt32(PlayerValue);
+                w = ValueConversion - 1;
+                if (ValueConversion < 1 || ValueConversion > 9)
                 {
                     Console.WriteLine("Не верная ячейка");
                 }
 
-                //Игрок 1
-                if (player % 2 != 0)
-
+                if (player % 2 == 0)
                 {
-                    
-                    switch (x)
-                    {
-                        case 1:
-                            game('X', 0, 0);
-                            break;
-                        case 2:
-                            game('X', 0, 1);
-                            break;
-                        case 3:
-                            game('X', 0, 2);
-                            break;
-                        case 4:
-                            game('X', 1, 0);
-                            break;
-                        case 5:
-                            game('X', 1, 1);
-                            break;
-                        case 6:
-                            game('X', 1, 2);
-                            break;
-                        case 7:
-                            game('X', 2, 0);
-                            break;
-                        case 8:
-                            game('X', 2, 1);
-                            break;
-                        case 9:
-                            game('X', 2, 2);
-                            break;
-                    }
+                    Games('0', w, mass);
                 }
-
-                //Игрое 2
-
                 else
+                {                   
+                    Games('X', w, mass);
+                }
+                if (
+                 mass[0] == 'X' & mass[1] == 'X' & mass[2] == 'X' |
+                 mass[3] == 'X' & mass[4] == 'X' & mass[5] == 'X' |
+                 mass[6] == 'X' & mass[7] == 'X' & mass[8] == 'X' |
+                 mass[2] == 'X' & mass[4] == 'X' & mass[6] == 'X' |
+                 mass[0] == 'X' & mass[4] == 'X' & mass[8] == 'X' |
+                 mass[0] == 'X' & mass[3] == 'X' & mass[6] == 'X' |
+                 mass[1] == 'X' & mass[4] == 'X' & mass[7] == 'X' |
+                 mass[2] == 'X' & mass[5] == 'X' & mass[8] == 'X')
                 {
-                    
-                    switch (x)
-                    {
-                        case 1:
-                            game('0', 0, 0);
-                            break;
-                        case 2:
-                            game('0', 0, 1);
-                            break;
-                        case 3:
-                            game('0', 0, 2);
-                            break;
-                        case 4:
-                            game('0', 1, 0);
-                            break;
-                        case 5:
-                            game('0', 1, 1);
-                            break;
-                        case 6:
-                            game('0', 1, 2);
-                            break;
-                        case 7:
-                            game('0', 2, 0);
-                            break;
-                        case 8:
-                            game('0', 2, 1);
-                            break;
-                        case 9:
-                            game('0', 2, 2);
-                            break;
-                    }
-
+                    Console.WriteLine("Выиграли: Х");
+                    break;
                 }
 
-                if (mass[0, 0] == 'X' && mass[0, 1] == 'X' && mass[0, 2] == 'X' ||
-                    mass[1, 0] == 'X' && mass[1, 1] == 'X' && mass[1, 2] == 'X' ||
-                    mass[2, 0] == 'X' && mass[2, 1] == 'X' && mass[2, 2] == 'X' ||
-                    mass[0, 0] == 'X' && mass[1, 0] == 'X' && mass[2, 0] == 'X' ||
-                    mass[0, 1] == 'X' && mass[1, 1] == 'X' && mass[2, 1] == 'X' ||
-                    mass[0, 2] == 'X' && mass[1, 2] == 'X' && mass[2, 2] == 'X' ||
-                    mass[2, 0] == 'X' && mass[1, 1] == 'X' && mass[0, 2] == 'X' ||
-                    mass[0, 0] == 'X' && mass[1, 1] == 'X' && mass[2, 2] == 'X')
+                if (
+                 mass[0] == '0' & mass[1] == '0' & mass[2] == '0' |
+                 mass[3] == '0' & mass[4] == '0' & mass[5] == '0' |
+                 mass[6] == '0' & mass[7] == '0' & mass[8] == '0' |
+                 mass[2] == '0' & mass[4] == '0' & mass[6] == '0' |
+                 mass[0] == '0' & mass[4] == '0' & mass[8] == '0' |
+                 mass[0] == '0' & mass[3] == '0' & mass[6] == '0' |
+                 mass[1] == '0' & mass[4] == '0' & mass[7] == '0' |
+                 mass[2] == '0' & mass[5] == '0' & mass[8] == '0')
 
-                    Console.WriteLine("Выиграли: Х");
-
-                if (mass[0, 0] == '0' && mass[0, 1] == '0' && mass[0, 2] == '0' ||
-                    mass[1, 0] == '0' && mass[1, 1] == '0' && mass[1, 2] == '0' ||
-                    mass[2, 0] == '0' && mass[2, 1] == '0' && mass[2, 2] == '0' ||
-                    mass[0, 0] == '0' && mass[1, 0] == '0' && mass[2, 0] == '0' ||
-                    mass[0, 1] == '0' && mass[1, 1] == '0' && mass[2, 1] == '0' ||
-                    mass[0, 2] == '0' && mass[1, 2] == '0' && mass[2, 2] == '0' ||
-                    mass[2, 0] == '0' && mass[1, 1] == '0' && mass[0, 2] == '0' ||
-                    mass[0, 0] == '0' && mass[1, 1] == '0' && mass[2, 2] == '0')
-
+                {
                     Console.WriteLine("Выиграли: 0");
-
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="PlayerSimbol">Подставляется символ игрока который ходит в данный момент.</param>
+        /// <param name="PontNumber">Подставляется номер ячейки массива. Берется из расчета значение вводимое игроком, минус один.</param>
+        /// <param name="mass">Параметр передающий значения полученые в методе массиву Main.</param>
+        //Создание метода
+        static void Games(char PlayerSimbol, int PontNumber, char[] mass)
+        {
+            if (mass[PontNumber] == 'X' | mass[PontNumber] == '0')
+            {
+                Console.WriteLine("Ячейка занята");
+            }
+            else
+            {
+                mass[PontNumber] = PlayerSimbol;
+                player++;
 
             }
-
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write($"|_{mass[i]}_");
+                if (i % 3 == 2)
+                {
+                    Console.Write("|");
+                    Console.Write("\n");
+                }
+            }
         }
     }
 }
+
